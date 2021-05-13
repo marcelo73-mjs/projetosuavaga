18a4562b2170021e5b785275b2fdb9bb9ed68ce6
marcelo73-mjs <57647187+marcelo73-mjs@users.noreply.github.com>
2021-05-13 15:20:56 -0300

    Criando o testePull.js

---
+++ testePull.js
@@ -0,0 +1,5 @@
+Este arquivo é somente para teste, pois estou testando com ele a funcionalidade PULL, onde posso add no meu repositório remoto local!!
+Sequencia: 
+Criar arquivo
+Commitar
+Fazer o pull no repositorio local
